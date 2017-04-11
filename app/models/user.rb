25cffc49c5bb8868c50c57de99ed94872b2c0e3b
Matt Smrke <matthew.smrke@empire.ca>
2017-04-11 18:33:58 -0400

    deletes everything

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  include Clearance::User
-  has_one :account
-  before_create :build_account
-end
--- test/factories/users.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:email) { |n| "test_user_#{n}@example.com" }
-    password "pass123"
-  end
-end
