07bd63aca3c8ff89bbd24461771afeca53621db0
Matt Smrke <matthew.smrke@empire.ca>
2017-04-11 18:39:41 -0400

    deletes stuff

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  include Clearance::User
-
-  has_many :health_statuses
-
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
-end
