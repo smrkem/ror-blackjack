83b5624234847c09a527cfa71ae0cfa60596416e
matt <smrkem@gmail.com>
2017-04-21 11:16:35 -0400

    reverts routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   resources :goals
   resources :health_statuses
 
-  resources :users, controller: :users, only: :create
+
+  resources :users, controller: :users, only: :create do
+      resource :password,
+        controller: "clearance/passwords",
+        only: [:create, :edit, :update]
+  end
 
   get "/confirm_email/:token" => "email_confirmations#update", as: "confirm_email"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
