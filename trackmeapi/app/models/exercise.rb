e81aecaedee2618f4aedf899f1d0d908bd94141d
matt <smrkem@gmail.com>
2017-10-06 13:37:30 -0400

    new rails app

--- trackmeapi/app/models/exercise.rb
+++
@@ -1,4 +0,0 @@
-class Exercise < ApplicationRecord
-  # validations
-  validates_presence_of :performed_at, :duration_minutes,:avg_heart_rate, :created_by
-end
--- trackmeapi/spec/models/exercise_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Exercise, type: :model do
-  it { should validate_presence_of(:performed_at) }
-  it { should validate_presence_of(:duration_minutes) }
-  it { should validate_presence_of(:avg_heart_rate) }
-  it { should validate_presence_of(:created_by) }
-end
